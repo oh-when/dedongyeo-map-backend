1a0835dc57a67156c64eb89c7715ea2e4e9f37a7
minkj1992 <minkj1992@gmail.com>
2021-05-01 21:07:13 +0900

    refactor: place 예외

--- src/place/kakaoMapSearch/search.service.ts
+++ src/place/kakaoMapSearch/search.service.ts
@@ -1,59 +1,44 @@
-import Axios from "axios";
-import { Injectable, HttpException, HttpStatus } from "@nestjs/common";
-import { ConfigService } from "@nestjs/config";
-import { CreateSpotInput } from "../../spot/dto/create-spot.input";
-import { KeywordSearchDto } from "./search.dto";
-import { PaginatedPlace, Place } from "../place.entity";
-import { PageInfo } from "../../shared/entities/pageinfo.entity";
-import { PageService } from "../../shared/page.service";
-import { SortType } from "../../place/kakaoMapSearch/search.dto";
+import Axios from 'axios';
+import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { CreateSpotInput } from '../../spot/dto/create-spot.input';
+import { KeywordSearchDto } from './search.dto';
+import { PaginatedPlace, Place } from '../place.entity';
+import { PageInfo } from '../../shared/entities/pageinfo.entity';
+import { PageService } from '../../shared/page.service';
+import { SortType } from '../../place/kakaoMapSearch/search.dto';
+import { KakaoApiServerError, PlaceNotFoundException, PlaceNotFoundIdenticalException } from 'src/shared/exceptions';
 
 @Injectable()
 export class SearchService {
-  constructor(
-    private configService: ConfigService,
-    private pageService: PageService
-  ) {}
+  constructor(private configService: ConfigService, private pageService: PageService) {}
 
   // https://developers.kakao.com/docs/latest/ko/local/dev-guide#search-by-keyword
-  async searchByKeyword(
-    keywordSearchDto: KeywordSearchDto
-  ): Promise<PaginatedPlace> {
-    const baseUrl = this.configService.get("app.KAKAO_DEV_HOST");
+  async searchByKeyword(keywordSearchDto: KeywordSearchDto): Promise<PaginatedPlace> {
+    const baseUrl = this.configService.get('app.KAKAO_DEV_HOST');
 
     const response: {
       meta: PageInfo;
       documents: Place[];
     } = await Axios.get(baseUrl, {
       headers: {
-        Authorization: `KakaoAK ${this.configService.get(
-          "app.KAKAO_DEV_REST_API_KEY"
-        )}`,
+        Authorization: `KakaoAK ${this.configService.get('app.KAKAO_DEV_REST_API_KEY')}`,
       },
       params: {
         ...keywordSearchDto,
       },
     })
-      .then((response) => response.data)
-      .catch((err) => {
+      .then(response => response.data)
+      .catch(err => {
         if (err.response.status == 400) {
-          throw new HttpException("no matched place", HttpStatus.BAD_REQUEST);
-        } else {
-          throw new HttpException(
-            "kakao api server error",
-            HttpStatus.INTERNAL_SERVER_ERROR
-          );
+          throw new PlaceNotFoundException(keywordSearchDto);
         }
+        throw new KakaoApiServerError();
       });
 
     const { total_count, is_end } = response.meta;
     const { size, page } = keywordSearchDto;
-    const pageInfo: PageInfo = this.pageService.getPageInfo(
-      size,
-      page,
-      total_count,
-      is_end
-    );
+    const pageInfo: PageInfo = this.pageService.getPageInfo(size, page, total_count, is_end);
 
     const paginatedPlace: PaginatedPlace = {
       pageInfo,
@@ -63,11 +48,7 @@ export class SearchService {
     return paginatedPlace;
   }
 
-  async getIdenticalPlace(
-    createSpotInput: CreateSpotInput
-  ): Promise<Place | null> {
-    console.log(createSpotInput);
-
+  async getIdenticalPlace(createSpotInput: CreateSpotInput): Promise<Place | null> {
     return this.searchByKeyword({
       query: createSpotInput.place_name,
       x: createSpotInput.x,
@@ -75,15 +56,9 @@ export class SearchService {
       radius: 1,
       sort: SortType.distance,
     })
-      .then(({ places }) => {
-        console.log(places);
-        return places.length >= 1 ? places[0] : null;
-      })
-      .catch((error) => {
-        throw new HttpException(
-          `cannot get identical place cause of ${error.message}`,
-          HttpStatus.INTERNAL_SERVER_ERROR
-        );
+      .then(({ places }) => (places.length >= 1 ? places[0] : null))
+      .catch(error => {
+        throw new PlaceNotFoundIdenticalException(createSpotInput.place_name);
       });
   }
 }
--- src/shared/exceptions.ts
+++ src/shared/exceptions.ts
@@ -1,6 +1,29 @@
 import { BadRequestException, InternalServerErrorException } from '@nestjs/common';
 import { KeywordSearchDto } from '../place/kakaoMapSearch/search.dto';
 
+export class PlaceNotFoundException extends BadRequestException {
+  constructor(keywordSearchDto: KeywordSearchDto) {
+    super(`Kakao Place Not Found.\r\n SearchDto Info: ${keywordSearchDto}`);
+  }
+}
+
+export class PlaceNotFoundIdenticalException extends BadRequestException {
+  /* Identical한 Place를 찾지 못할 때 발생.
+   * 카카오 API에는 Retrieve가 구현되어있지 않고 있다.
+   * 이 때문에 클라이언트에 제공했던 place에 사용자가 최초로 스티커를 붙일 경우, 다시 place를 검색해주어야 한다.
+   * 이때 place명과 coordinate를 조합해 쿼리하는데 API로 데이터를 찾지 못할 경우 해당 에러를 보내준다.
+   */
+  constructor(placeName: string) {
+    super(`There is no identical Kakao Place(${placeName})`);
+  }
+}
+
+export class KakaoApiServerError extends InternalServerErrorException {
+  constructor() {
+    super(`Kakao API server error`);
+  }
+}
+
 export class CourseNotFoundException extends BadRequestException {
   constructor(courseId: String) {
     super(`Course(id: ${courseId}) Not Found`);
